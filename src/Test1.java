f449ee45f5f4bb950ab67254afd0543b55ba4204
YogeshX123 <yogeshstrikes518@gmail.com>
2025-03-12 12:29:42 +0530

    Update Test1.java

--- src/Test1.java
+++ src/Test1.java
@@ -1,17 +1,24 @@
 
 
 public class Test1 {
-	
-	public void yogesh() {
+    
+    // Method that prints out elements of the array
+    public void yogesh() {
+        int[] a = {10, 20, 30, 50};
+        
+        // Loop through the array and print each element
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
 
-		int[] a= {10,20,30,50};
-		
-		for(int i=0; i< a.length; i++) {
-			
-			System.out.println(a[i]);
-			
-		}
-
-	}
-	
+    // Main method to run the program
+    public static void main(String[] args) {
+        // Create an instance of Test1
+        Test1 test = new Test1();
+        
+        // Call the yogesh method to run the code
+        test.yogesh();
+    }
 }
+
